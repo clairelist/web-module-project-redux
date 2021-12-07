6d7efd277f43cbbb178b0818167434cb68800294
Claire List <claire.list2501@gmail.com>
2021-12-07 16:52:03 -0700

    delete functionality working!

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 
 import {connect} from 'react-redux';
+import {deleteMovie} from './../actions/movieActions';
 
 const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const {movies} = props;
+    const {movies,deleteMovie} = props;
     const movie = movies.find(movie=>movie.id===Number(id));
+
+    //NOW  we need a handler which will do the deleting!
+    const handleDelete = () =>{
+        deleteMovie(movie.id);
+        push('/movies');
+    }
     
     return(<div className="modal-page col">
         <div className="modal-dialog">
@@ -40,7 +47,7 @@ const Movie = (props) => {
                         
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete" onClick={handleDelete}><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
                 </div>
@@ -55,4 +62,4 @@ const mapStateToProps=(state)=>{
     });
 }
 
-export default connect(mapStateToProps)(Movie);
+export default connect(mapStateToProps,{deleteMovie})(Movie);
