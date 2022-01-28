61a9fac4db9952a24861f4202272d656f3cb4142
Claire List <claire.list2501@gmail.com>
2022-01-28 15:27:05 -0700

    rework

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -24,7 +24,7 @@ const AddMovieForm = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault(); //ALWAYS prevent default!!
-        props.addMovie(movie);
+       props.addMovie(movie);
         push('/movies');
     }
 
